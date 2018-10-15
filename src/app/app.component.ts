658ce1372152b0dcac951ec38bc0aab8550758e2
Oleg Rovenskyi <oleg.rovenskyi@dev-pro.net>
2018-10-15 12:20:26 +0300

    remove old files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,11 +18,7 @@ import { AppState } from './app.service';
   templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
-  public name = 'Angular Starter';
-  public tipe = 'assets/img/tipe.png';
-  public twitter = 'https://twitter.com/gdi2290';
-  public url = 'https://tipe.io';
-  public showDevModule: boolean = environment.showDevModule;
+  public name = 'Effect Example';
 
   constructor(
     public appState: AppState
--- src/app/shared/store-utils/store-utils.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { Action } from '@ngrx/store';
-
-@Injectable()
-export class StoreUtilsService {
-  public createAction(type, payload?): Action {
-    return { type };
-  }
-}

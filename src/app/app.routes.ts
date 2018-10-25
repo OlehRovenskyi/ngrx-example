b199ac87dacf427abb2afd2ff4981d77ed15b879
oleg.rovenskyi <$Money$%$7>
2018-10-25 14:54:18 +0300

    clean

--- src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { CounterComponent } from 'app/counter';
-
-export const ROUTES: Routes = [
-  { path: '',      component: CounterComponent },
-  { path: '**',    component: CounterComponent },
-];
